f2510a44373ce7cbf70292a65e7e7bbb54dac26a
Elliot Hoang <lochoang75@gmail.com>
2021-03-20 22:05:46 +0700

    add example for octalconstant

---
+++ octalconstantExample.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define DECIMAL_CONSTANT    52
+#define OCTAL_CONSTANT      052
+
+int main(int argc, char *argv[]) {
+    printf("Decimal constant %d\r\n", DECIMAL_CONSTANT);
+    printf("Octal constant value %d\r\n", OCTAL_CONSTANT);
+    return 0;
+}
